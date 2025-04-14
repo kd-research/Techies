af4ae00c0753a804722651466428cfa1ff4fdc28
rsarder <-ridu321@gmail>
2025-04-13 20:39:47 -0400

    merge from upstream

--- techies/tools.py
+++ techies/tools.py
@@ -59,7 +59,7 @@ def get_all_tools():
     # Register all built-in tools
     tool_classes = [
         ReadFileTool, BatchReadFilesTool, WriteFileTool, ListFilesTool,
-        SaveSoundTool, SearchSoundTool, ReadHtmlExamplesTool
+        SaveSoundTool, SearchSoundTool, ReadHtmlExamplesTool, QueryMechanicsTool, GoogleSearchTool,
     ]
     
     for tool_class in tool_classes:
